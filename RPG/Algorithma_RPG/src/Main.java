838dad372ebc6475a180876a6bf3421174ae4c42
Enzo de Oliveira <develop.silversol@gmail.com>
2025-10-19 17:14:59 -0300

    Segundo comentario

--- RPG/Algorithma_RPG/src/Main.java
+++ RPG/Algorithma_RPG/src/Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("Inicio da implementacao do jogo...");
+        System.out.println("mudei memes");
+        
+        
     } 
 }
