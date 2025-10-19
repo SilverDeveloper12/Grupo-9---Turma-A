05d05c5c3664cd170d4594cfed51d4ae6a9f0b47
Enzo de Oliveira <develop.silversol@gmail.com>
2025-10-19 17:30:31 -0300

    Alteracao do nome da classe para 'Jogo'

--- RPG/Algorithma_RPG/src/Jogo.java
+++ RPG/Algorithma_RPG/src/Jogo.java
@@ -1,4 +1,4 @@
-public class Main{
+public class Jogo{
     public static void main(String[] args){
         System.out.println("Inicio da implementacao do jogo...");
         System.out.println("mudei memes");
